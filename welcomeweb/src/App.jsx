554c9a9ace6655876431067b2e0c08761459ccc2
Abhinash@2004 <bejabhinash@gmail.com>
2025-04-05 17:57:40 +0530

    created the nav bar

--- welcomeweb/src/App.jsx
+++ welcomeweb/src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <nav className='flex bg-black text-white'>
+      <p>SecuResidences</p>
+      <ul className='text-green-700 flex ml-10'>
+        <li>Home</li>
+        <li>Features</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Survey</li>
+      </ul>
+      <button className='btn'>Download App</button>
+    </nav>
   )
 }
 
