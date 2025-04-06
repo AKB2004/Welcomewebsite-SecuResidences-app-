ff0a414f4298a9ef7c55974a6e99996208107a63
Abhinash@2004 <bejabhinash@gmail.com>
2025-04-06 22:42:12 +0530

    completed the contact page and intgretated using backend

--- welcomeweb/src/components/Survey.jsx
+++ welcomeweb/src/components/Survey.jsx
@@ -4,7 +4,7 @@
 // const Survey = () => {
 //   return (
 //     <div className="p-6 text-center">
-//       <h1 className="text-3xl font-bold text-purple-700 mb-4">Give Us Your Feedback</h1>
+//       <h1 className="mb-4 text-3xl font-bold text-purple-700">Give Us Your Feedback</h1>
 //       <iframe
 //         src="https://docs.google.com/forms/d/e/1FAIpQLSe9MSrK0oD4RVjfHdUajXWiuIDXdbgC6XeSnBgqMeDNVn-HKw/viewform?usp=dialog "
 //         width="100%"
@@ -18,17 +18,19 @@
 
 // export default Survey;
 import React, { useState } from "react";
+// import Contact from "./Contact";
+
 
 const Survey = () => {
   const [loaded, setLoaded] = useState(false);
 
   return (
     <div className="min-h-screen px-6 py-16 text-center bg-white">
-      <h1 className="text-3xl font-bold mb-4">We Value Your Opinion</h1>
+      <h1 className="mb-4 text-3xl font-bold">We Value Your Opinion</h1>
 
       {!loaded && (
         <div className="flex justify-center items-center h-[600px]">
-          <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-purple-600 border-opacity-50"></div>
+          <div className="w-16 h-16 border-t-4 border-purple-600 border-opacity-50 rounded-full animate-spin"></div>
         </div>
       )}
 
